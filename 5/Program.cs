ab230e7450132244777e96ffb7483a1b56ed7787
ander <ander@cjoanbatz@gmail.com>
2026-04-13 13:53:25 -0600

    Termine el ejercicio 5

---
+++ 5/Program.cs
@@ -0,0 +1,17 @@
+﻿class Usuario
+{
+    private string password;
+    public string Password
+    {
+        get
+        {
+            int contado = 0;
+            foreach (char c in password)
+            {
+                contado++;
+            }
+            if (contado >= 8) return "****";
+            else return "Contraseña insegura";
+        }
+    }
+}
