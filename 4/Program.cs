623df7f9f7e2f2737f616a065783c4c4e2243c3f
ander <ander@cjoanbatz@gmail.com>
2026-04-13 13:46:02 -0600

    Terminé el ejercicio 4

---
+++ 4/Program.cs
@@ -0,0 +1,12 @@
+﻿class Empleado
+{
+    private double salario;
+    public string Salario
+    {
+        get
+        {
+            if (salario > 3843.55) return $"Salario: {salario}";
+            else return "Salario inválido";
+        }
+    }
+}
