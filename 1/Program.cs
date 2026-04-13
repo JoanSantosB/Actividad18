6b7d9cca20b98d85e6b8386d95025879df22ce84
ander <ander@cjoanbatz@gmail.com>
2026-04-13 13:22:16 -0600

    Agregar archivos de proyecto.

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Channels;
+
+class CuentaBancaria
+{
+    private double saldo;
+    public string Saldo
+    {
+        get
+        {
+            if (saldo > 0) return $"Saldo disponible: {saldo}";
+            else return "Saldo no disponible";
+        }
+    }
+}
