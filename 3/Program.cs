80d8bc65f33b58e814095569845826c5705d476c
ander <ander@cjoanbatz@gmail.com>
2026-04-13 13:37:11 -0600

    Terminé el ejercicio 3

---
+++ 3/Program.cs
@@ -0,0 +1,12 @@
+﻿class Producto
+{
+    private double precio;
+    public double Precio
+    {
+        get
+        {
+            if (precio >=0) return precio;
+            else return 0;
+        }
+    }
+}
