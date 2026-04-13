6cb4ca7527cc7b2dbee102d8a11525085d2e5877
ander <ander@cjoanbatz@gmail.com>
2026-04-13 13:31:45 -0600

    Terminé el ejercicio 2

---
+++ 2/Program.cs
@@ -0,0 +1,12 @@
+﻿class Estudiante
+{
+    private int edad;
+    public int Edad
+    {
+        get
+        {
+            if (edad >= 0&&edad<=100) return edad;
+            else return 0;
+        }
+    }
+}
